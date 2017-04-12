9b2b72709786dd32d279ca4b13c28f1b810c84f6
wangshengxing <1048381213@qq.com>
2017-04-12 10:48:58 +0800

    add some class

---
+++ app/src/main/java/com/aispeech/voicerecognitionanimation/VoiceWaterView.java
@@ -0,0 +1,31 @@
+package com.aispeech.voicerecognitionanimation;
+
+/**
+ * author       : wangshengxing;
+ * date         : 2017/4/11;
+ * email        : wangshengxing@haloai.com;
+ * package_name : com.aispeech.voicerecognitionanimation;
+ * project_name : VoiceRecognitionAnimation;
+ */
+public class VoiceWaterView implements IVoiceStatusViewLister{
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onRecognize() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onShow() {
+
+    }
+}
