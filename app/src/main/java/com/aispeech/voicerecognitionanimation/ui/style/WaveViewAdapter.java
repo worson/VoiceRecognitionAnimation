6594308ef523aad0502d96b7b456a77c559835e3
dell-PC\dell <1048381213@qq.com>
2017-04-17 18:38:07 +0800

    nothing

---
+++ app/src/main/java/com/aispeech/voicerecognitionanimation/ui/style/WaveViewAdapter.java
@@ -0,0 +1,44 @@
+package com.aispeech.voicerecognitionanimation.ui.style;
+
+import com.aispeech.voicerecognitionanimation.voice.IVoiceStatusViewLister;
+
+/**
+ * author       : wangshengxing;
+ * date         : 2017/4/11;
+ * email        : wangshengxing@haloai.com;
+ * package_name : com.aispeech.voicerecognitionanimation;
+ * project_name : VoiceRecognitionAnimation;
+ */
+public class VoiceWaterView implements IVoiceStatusViewLister {
+
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onRecognize() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onShow() {
+
+    }
+
+    @Override
+    public void onTTS() {
+
+    }
+
+    @Override
+    public void onUpdateVolume(int vol) {
+
+    }
+}
